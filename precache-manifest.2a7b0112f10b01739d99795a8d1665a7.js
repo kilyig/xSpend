9f681f621015d5b7f4b20e05d8b427ca5881f580
kilyig <yigit.kilicoglu@yale.edu>
2022-07-04 13:17:00 +0200

    Updates

---
+++ precache-manifest.2a7b0112f10b01739d99795a8d1665a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9e791abc9d9d41d45af89b7cc9d4ac",
+    "url": "/xSpend/index.html"
+  },
+  {
+    "revision": "b6203df45900c4f1343e",
+    "url": "/xSpend/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "b6203df45900c4f1343e",
+    "url": "/xSpend/static/js/2.adae9717.chunk.js"
+  },
+  {
+    "revision": "d57dae176fb87a393a80e02d0e438720",
+    "url": "/xSpend/static/js/2.adae9717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6488bbb311cc96a0336",
+    "url": "/xSpend/static/js/main.a8549311.chunk.js"
+  },
+  {
+    "revision": "25891d47fc8242990cbe",
+    "url": "/xSpend/static/js/runtime-main.7d458d44.js"
+  },
+  {
+    "revision": "1975704f5053ab8c5a10b9722b94d972",
+    "url": "/xSpend/static/media/prosperity.1975704f.svg"
+  },
+  {
+    "revision": "c2f3b670e879f08ae101ffb68b72db7a",
+    "url": "/xSpend/static/media/xspend_logo.c2f3b670.png"
+  }
+]);
